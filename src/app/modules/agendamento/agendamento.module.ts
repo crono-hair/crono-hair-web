a5f7b333723921b2ae87274546a752d61539c870
Noemi1 <54690296+Noemi1@users.noreply.github.com>
2022-06-20 18:31:43 -0300

    aaa

---
+++ src/app/modules/agendamento/agendamento.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AgendamentoRoutingModule } from './agendamento-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AgendamentoRoutingModule
+  ]
+})
+export class AgendamentoModule { }
---
+++ src/app/modules/funcionarios/funcionarios.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FuncionariosRoutingModule } from './funcionarios-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FuncionariosRoutingModule
+  ]
+})
+export class FuncionariosModule { }
